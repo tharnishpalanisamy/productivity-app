35939ca68d0186c3304f68603829bda27924e1dc
Tharnish Palanisamy <tharnishpalanisamy3@gmail.com>
2026-07-29 16:01:16 +0530

    init

---
+++ scripts/app.js
@@ -0,0 +1,81 @@
+const heatmapData = [];
+
+const startDate = new Date(2026, 0, 1);
+const today = new Date(2026, 6, 29);
+
+for (
+    let date = new Date(startDate);
+    date <= today;
+    date.setDate(date.getDate() + 1)
+) {
+    heatmapData.push({
+        date: date.toISOString().split("T")[0],
+        completedTasks: Math.floor(Math.random() * 9)
+    });
+}
+
+function createHeatMap(){
+    let gridContainer = document.querySelector('.grid-container') 
+    gridContainer.innerHTML =''
+    heatmapData.forEach(data =>{
+        let date = new Date(data.date)
+        const fullMonth = date.toLocaleString('default', { month: 'long' }); 
+        let contribution = data.completedTasks > 5 ? 'high' : 
+        data.completedTasks > 1 ? 'medium' : data.completedTasks == 1 ? 'low' : 'zero' 
+        
+        gridContainer.innerHTML += `
+        <div class = 'block ${contribution }' 
+        data-bs-toggle="tooltip" data-bs-html="true" 
+        data-bs-title="
+        <p class = 'contribution-tooltip'>
+            ${data.completedTasks} contributions on ${fullMonth} ${date.getDate()}th
+        </p>"
+        > </div>
+        ` 
+    })
+}
+createHeatMap()
+
+
+console.log(heatmapData);
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => 
+    new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+
+
+const profileImg = document.getElementById('profileImage');
+const profileImgInput = document.getElementById('profileImageInput');
+const chooseProfileImg = document.querySelector('.smile-logo-container');
+
+let user = JSON.parse(localStorage.getItem('user')) || {};
+
+if (user.img) {
+    profileImg.src = user.img;
+}
+
+chooseProfileImg.addEventListener('click', () => {
+    profileImgInput.click();
+});
+
+profileImgInput.addEventListener('change', () => {
+    const file = profileImgInput.files[0];
+
+    if (!file){
+        return
+    }
+
+    const reader = new FileReader()
+
+    reader.onload = () => {
+        profileImg.src = reader.result
+        user.img = reader.result
+        localStorage.setItem('user', JSON.stringify(user))
+    };
+
+    reader.readAsDataURL(file)
+}); 
